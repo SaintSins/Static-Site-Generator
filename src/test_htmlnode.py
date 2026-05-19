59189c829268c6d621001cfb14e24219ef5aaf9b
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-05-20 00:20:24 +0530

    Implementing LeafNode and it's test cases

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple(self):
@@ -13,3 +13,22 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_none(self):
         node = HTMLNode()
         self.assertEqual(node.props_to_html(), "")
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("Hello, world!", "p")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_props(self):
+        node = LeafNode("Click me!", "a", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode("Just some plain text.")
+        self.assertEqual(node.to_html(), "Just some plain text.")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode(None, "p")
+        with self.assertRaises(ValueError):
+            node.to_html()
