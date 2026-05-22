1b214c9a440e8a9e5dc7ae555bf2c35be97124a7
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-05-22 13:37:15 +0530

    Implementing Split Delimeter and it's test cases

---
+++ src/inline_markdown.py
@@ -0,0 +1,21 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        splited_node = node.text.split(delimiter)
+        if len(splited_node)%2 == 0: #Length of the splitted node will be in odd number if closing delimeter is present
+            raise Exception("Invalid markdown, formatted section not closed.")
+        for i, part in enumerate(splited_node):
+            if part == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+        return new_nodes
+
+        
