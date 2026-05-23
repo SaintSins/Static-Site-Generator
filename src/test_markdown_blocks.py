187b8b38e7746c4462a79f4e3880800fa14976cd
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-05-24 01:09:51 +0530

    Implementing markdown to block function

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,65 @@
+import unittest
+from markdown_blocks import markdown_to_blocks
+
+class TestMarkdownBlocks(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+            ],
+        )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is a paragraph with excessive newlines.
+
+
+
+
+
+This is another paragraph.
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is a paragraph with excessive newlines.",
+                "This is another paragraph.",
+            ],
+        )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+if __name__ == "__main__":
+    unittest.main()
