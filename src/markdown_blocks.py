9e5009441b2bb43b703648e6791876f409d2ee5e
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-05-24 23:09:59 +0530

    Implementing function to asses block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -17,3 +17,42 @@ def markdown_to_blocks(markdown):
             continue
         cleaned_blocks.append(stripped_block)
     return cleaned_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+        
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+        
+    is_quote = True
+    for line in lines:
+        if not line.startswith(">"):
+            is_quote = False
+            break
+    if is_quote:
+        return BlockType.QUOTE
+    
+    is_unordered = True
+    for line in lines:
+        if not line.startswith("- "):
+            is_unordered = False
+            break
+    if is_unordered:
+        return BlockType.UNORDERED_LIST
+    
+    is_ordered = True
+    counter = 1
+    for line in lines:
+        if not line.startswith(f'{counter}. '):
+            is_ordered = False
+            break
+        else:
+            counter += 1
+    if is_ordered:
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+        
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -60,6 +60,39 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_to_block_types(self):
+        block = "### This is a heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+        block = "```\nprint('hello world')\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+        block = "> This is a quote\n> on multiple lines"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+        block = "- Item 1\n- Item 2\n- Item 3"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+        block = "1. First\n2. Second\n3. Third"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+        block = "Just a normal paragraph of text."
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_sad_paths(self):
+        block = "###This is a paragraph now"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+        block = "> This is a quote\nbut this line ruins it"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+        block = "-Item 1\n-Item 2"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+        block = "1. First\n3. Third"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
