edcfadc18cc5e96ff8764fc5dfc96fc7c4cf4d61
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-05-28 22:26:51 +0530

    fix: add basepath as correct argument

--- src/main.py
+++ src/main.py
@@ -7,6 +7,6 @@ def main():
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
     copy_files_recursive("static","docs")
-    generate_pages_recursive("content", "template.html", "docs", "basepath")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     
 main()
